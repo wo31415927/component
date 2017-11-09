9e54939eba41424932e3ebf1dd2536a82d167bec
chenxiang <wo31415927@126.com>
2017-11-09 18:01:26 +0800

    add 动态代理

---
+++ src/main/java/jdk/reflect/Client.java
@@ -0,0 +1,23 @@
+package jdk.reflect;
+
+import java.lang.reflect.Proxy;
+
+/**
+ * zeyu
+ * 2017/11/9
+ */
+public class Client {
+    public static void main(String[] args) {
+        IPerson person = new Person("Jack", 0);
+        IPerson proxyPerson = (IPerson) Proxy.newProxyInstance(person.getClass().getClassLoader(), person.getClass().getInterfaces(), new OwnerHandler
+                (person));
+        try {
+            System.out.println(Proxy.isProxyClass(proxyPerson.getClass()));
+            System.out.println(proxyPerson.getName());
+            System.out.println(proxyPerson.getGender());
+            proxyPerson.setFlower(1);
+        }catch (Exception e){
+            System.out.println(e.getCause().getMessage());
+        }
+    }
+}
