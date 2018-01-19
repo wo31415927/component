cf057b8360900c61052cf13e58dbd30109c3a355
chenxiang <wo31415927@126.com>
2018-01-19 17:16:12 +0800

    hdfs pass

--- src/test/java/hdfs/HdfsClientTest.java
+++ src/test/java/hdfs/HdfsClientTest.java
@@ -6,33 +6,48 @@ import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
-import org.apache.hadoop.security.SecurityUtil;
 import org.apache.hadoop.security.UserGroupInformation;
+import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
 
 /**
  * @author zeyu 2018/1/17
  */
 public class HdfsClientTest {
-    public static final String KEYTAB_FILE_KEY = "hdfs.keytab.file";
-    public static final String USER_NAME_KEY = "hdfs.kerberos.principal";
+    public String host = "vm253";
+    public String realm = "VM.COM";
+    //namenode
+    public String uri = "hdfs://vm253:9000/";
+    public String keyPath = "C:\\Users\\zeyu\\Desktop\\HDFS\\hdfs.keytab";
+    public String principal = "hdfs/vm253@VM.COM";
+    Configuration conf = new Configuration();
 
-    public static void main(String[] args) throws Exception {
-        String uri = "hdfs://vm253:9000/";
-        Configuration conf = new Configuration();
-        //keytab文件的路径
-        conf.set(KEYTAB_FILE_KEY, "C:\\Users\\zeyu\\Desktop\\udaldump_test\\hdfs\\op.keytab");
-        //principal
-        conf.set(USER_NAME_KEY, "hdfs");
-        login(conf,conf);
-        //指定用户名连接HDFS，以获取linux用户对应的文件访问权限
-        //FileSystem fs = FileSystem.get(URI.create(uri), config,"hdfs");
-        //或者不使用用户，将hdfs对应的文件目录设置为777以便可以写入
+    public void login() throws IOException {
+        //可以直接设置以下属性，也可以设置krb5.conf
+        System.setProperty("java.security.krb5.realm",realm);
+        System.setProperty("java.security.krb5.kdc",host);
+        //System.setProperty("java.security.krb5.conf", krbPath);
+        //这里设置namenode
+        conf.set("fs.defaultFS", uri);
+        //需要增加hadoop开启了安全的配置
+        conf.setBoolean("hadoop.security.authorization", true);
+        //配置安全认证方式为kerberos
+        conf.set("hadoop.security.authentication", "Kerberos");
+        //设置namenode的principal
+        conf.set("dfs.namenode.kerberos.principal", principal);
+        //设置datanode的principal值为“hdfs/_HOST@YOU-REALM.COM”
+        conf.set("dfs.datanode.kerberos.principal", principal);
+        //通过hadoop security下中的 UserGroupInformation类来实现使用keytab文件登录
+        UserGroupInformation.setConfiguration(conf);
+        //设置登录的kerberos principal和对应的keytab文件，其中keytab文件需要kdc管理员生成给到开发人员
+        UserGroupInformation.loginUserFromKeytab(principal, keyPath);
+    }
+
+    public void access() throws IOException {
         //hdfs dfs -chmod -R 777 /test
-        FileSystem fs = FileSystem.get(URI.create(uri), conf);
+        FileSystem fs = FileSystem.get(conf);
         // 列出hdfs上/user/fkong/目录下的所有文件和目录
         FileStatus[] statuses = fs.listStatus(new Path("/"));
         for (FileStatus status : statuses) {
@@ -48,18 +63,9 @@ public class HdfsClientTest {
         IOUtils.copyBytes(is, System.out, 1024, true);
     }
 
-
-    public static void login(Configuration conf, Configuration hdfsConfig) throws IOException {
-        if (UserGroupInformation.isSecurityEnabled()) {
-            String keytab = conf.get(KEYTAB_FILE_KEY);
-            if (keytab != null) {
-                hdfsConfig.set(KEYTAB_FILE_KEY, keytab);
-            }
-            String userName = conf.get(USER_NAME_KEY);
-            if (userName != null) {
-                hdfsConfig.set(USER_NAME_KEY, userName);
-            }
-            SecurityUtil.login(hdfsConfig, KEYTAB_FILE_KEY, USER_NAME_KEY);
-        }
+    @Test
+    public void test1() throws Exception {
+        login();
+        access();
     }
 }
