4b763c903373b2da9302e8ca4b08eb7fdc270897
chenxiang <wo31415927@126.com>
2018-02-08 17:15:44 +0800

    add file test

--- src/test/java/jdk/io/MyFileTest.java
+++ src/test/java/jdk/io/MyFileTest.java
@@ -53,34 +53,71 @@ public class MyFileTest {
     }
 
     @Test
-    public void testLoadFile() throws Exception {
-        Path path = Paths.get("target/classes/log4j2.xml");
-        //使用决对路径或者从项目根目录获取
-        System.out.println(path.toFile().getAbsolutePath());
-        try (InputStream inputStream = Files.newInputStream(path)) {
-            System.out.println(inputStream);
-        }
-        //竟然无法使用绝对路径
-        try (InputStream inputStream =
-                     MyFile.class.getResourceAsStream("D:\\tmp\\mytmp\\apple1\\testcolumn\\testcolumn.txt")) {
-            System.out.println(inputStream);
-        }
-        try (InputStream inputStream =
-                     new FileInputStream("D:\\tmp\\mytmp\\apple1\\testcolumn\\testcolumn.txt")) {
-            System.out.println(inputStream);
-        }
-        //同File
+    public void testLoadFilePath() throws Exception {
+        //项目根目录获取
+        String filePath1 = "target/classes/log4j2.xml";
+        //从系统盘根目录获取
+        String filePath2 = "/target/classes/log4j2.xml";
+        //Path
+        System.out.println(Paths.get(filePath1).toFile().getAbsolutePath());
+        System.out.println(Paths.get(filePath2).toFile().getAbsolutePath());
+        //同Path
+        File f1 = new File(filePath1);
+        File f2 = new File(filePath2);
+        System.out.println(f1.getAbsolutePath());
+        System.out.println(f2.getAbsolutePath());
+        //同Path
         Files.walkFileTree(
-//                Paths.get("target/classes/"),
-                Paths.get("D:\\tmp\\mytmp\\apple1\\testcolumn"),
+                Paths.get("target/classes/"),
                 new SimpleFileVisitor<Path>() {
                     @Override
                     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
                             throws IOException {
-                        System.out.println(file.toFile().getAbsolutePath());
+//                        System.out.println(file.toFile().getAbsolutePath());
                         return super.visitFile(file, attrs);
                     }
                 });
+        //同Path
+        try (InputStream inputStream =
+                     new FileInputStream(filePath1)) {
+            System.out.println("inputStream1: " + inputStream);
+        }
+        try (InputStream inputStream =
+                     new FileInputStream(filePath2)) {
+            System.out.println("inputStream2: " + inputStream);
+        }catch (Exception e){}
+        //同Path
+        try (InputStream inputStream = Files.newInputStream(Paths.get(filePath1))) {
+            System.out.println("Channels.newInputStream1: " + inputStream);
+        }catch (Exception e){}
+        try (InputStream inputStream = Files.newInputStream(Paths.get(filePath2))) {
+            System.out.println("Channels.newInputStream2: " + inputStream);
+        }catch (Exception e){}
+        //调用的是classLoader，所以无法使用绝对路径
+        //Class.resolveName(name)
+        // 1)未带/,则转化为当前class所在的package1/xx/xx.class
+        // 2)带/,则去掉/
+        //然后交给class对应的classLoader加载，AppClassLoader在classpath下寻址，classpath包含classes与test-classes
+        try (InputStream inputStream =
+                     MyFile.class.getResourceAsStream("MyScannerTest.class")) {
+            System.out.println("class1: "+inputStream);
+        }catch (Exception e){}
+        try (InputStream inputStream =
+                     MyFile.class.getResourceAsStream("/zookeeper/curator/Executor.class")) {
+            System.out.println("class2: "+inputStream);
+        }catch (Exception e){}
+        //无法使用绝对路径
+        //classloader在classpath下寻址，xx.xx.xx,这种java包寻址的方式不支持，必须是类似目录与文件的寻址方式
+        try (InputStream inputStream =
+                     this.getClass().getClassLoader().getResourceAsStream("child/file.xml")) {
+            System.out.println("classLoader1: "+inputStream);
+        }catch (Exception e){}
+
+        try (InputStream inputStream =
+                     this.getClass().getClassLoader().getResourceAsStream("org/apache/commons/io/FileUtils.class")) {
+            System.out.println("classLoader2: "+inputStream);
+        }catch (Exception e){}
+
     }
 
     /**
--- src/test/java/jdk/io/MyScannerTest.java
+++ src/test/java/jdk/io/MyScannerTest.java
@@ -3,55 +3,125 @@ package jdk.io;
 import com.google.common.base.Splitter;
 import com.google.common.base.Strings;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.file.Paths;
 import java.util.Scanner;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
-/** @author zeyu 2018/2/7 */
+/**
+ * @author zeyu 2018/2/7
+ */
 public class MyScannerTest {
-  ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
-
-  @Test
-  public void testScanner() throws Exception {
+    final int[] preIndex = {0};
+    final long[] preSpace = {0};
+    ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
     int period = 5;
     String path = "k://big/queue.csv";
     String delimiter = "\n";
+    int bufferSize = 1024 * 1024;
+    int size1MB = 1024 * 1024;
+    //rows
     AtomicInteger index = new AtomicInteger();
-    final int[] preIndex = {0};
+    //bytes
+    AtomicLong space = new AtomicLong();
     Splitter splitter = Splitter.on("|");
-    executorService.scheduleAtFixedRate(
-        new Runnable() {
-          @Override
-          public void run() {
-            int curIndex = index.get();
-            System.out.println("Speed(rows/s): " + (curIndex - preIndex[0]) / (double) period);
-            preIndex[0] = curIndex;
-          }
-        },
-        0,
-        period,
-        TimeUnit.SECONDS);
-    try (Scanner reader = new Scanner(Paths.get(path))) {
-      reader.useDelimiter(delimiter);
-      String row;
-      //略过空行
-      //xx&xx&  第2个&后面reader.hasNext==false
-      while (reader.hasNext() && !Strings.isNullOrEmpty(row = reader.next())) {
+
+    private void statics() {
+        executorService.scheduleAtFixedRate(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        int curIndex = index.get();
+                        long curSpace = space.get();
+                        System.out.println(String.format("Speed:%s(rows/s); %s(M/s)", (curIndex - preIndex[0]) / (double) period, (curSpace -
+                                preSpace[0]) /
+                                1024.0 / 1024.0 / period));
+                        preIndex[0] = curIndex;
+                        preSpace[0] = curSpace;
+                    }
+                },
+                0,
+                period,
+                TimeUnit.SECONDS);
+    }
+
+    private void processRow(String row) throws InterruptedException {
         if (index.incrementAndGet() <= 0) {
-          continue;
+            return;
         }
         if (Thread.interrupted()) {
-          throw new InterruptedException();
+            throw new InterruptedException();
         }
         if (!Strings.isNullOrEmpty(row)) {
-          splitter.splitToList(row);
+            splitter.splitToList(row);
+            space.addAndGet(row.getBytes().length);
         }
-      }
     }
-  }
+
+    @Test
+    public void testScanner() throws Exception {
+        statics();
+        try (Scanner reader = new Scanner(Paths.get(path))) {
+            /*Field buf = Scanner.class.getDeclaredField("buf");
+            buf.setAccessible(true);
+            buf.set(reader, CharBuffer.allocate(bufferSize));*/
+            reader.useDelimiter(delimiter);
+            String row;
+            //略过空行
+            //xx&xx&  第2个&后面reader.hasNext==false
+            while (reader.hasNext() && !Strings.isNullOrEmpty(row = reader.next())) {
+                processRow(row);
+            }
+        }
+    }
+
+    @Test
+    public void testLineIterator() throws Exception {
+        statics();
+        LineIterator it = FileUtils.lineIterator(new File(path));
+        String row;
+        try {
+            while (it.hasNext() && !Strings.isNullOrEmpty(row = it.nextLine())) {
+                processRow(row);
+            }
+        } finally {
+            LineIterator.closeQuietly(it);
+        }
+    }
+
+    @Test
+    public void testBufferReader() throws Exception {
+        statics();
+        String row;
+        try (BufferedReader reader = new BufferedReader(new FileReader(path),1024*1024*36)) {
+            while (-1 != reader.read()){
+                //
+            }
+            /*while (!Strings.isNullOrEmpty(row = reader.readLine())) {
+                processRow(row);
+            }*/
+        }
+    }
+
+    @Test
+    public void testMappedBuffer() throws Exception {
+        RandomAccessFile memoryFile = new RandomAccessFile("", "rw");
+        MappedByteBuffer mappedByteBuffer = memoryFile.getChannel().map(FileChannel.MapMode.READ_ONLY, 0, size1MB);
+        for (int i = 0; i < size1MB; i++) {
+            mappedByteBuffer.get(i);
+        }
+    }
 }
